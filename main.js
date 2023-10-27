28fde0792fe20428bef56b5c210c189fdd8bfaf0
loyara15 <104777662+loyara15@users.noreply.github.com>
2023-10-27 16:14:18 -0300

    nbcngcngfgn

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclik = tocaSomPom;
+document.querySelector('.tecla tecla_pom').onclik = tocaSomPom;

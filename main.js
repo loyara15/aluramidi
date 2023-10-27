6f169584c47f766eecba40a1caed7572a413e602
loyara15 <104777662+loyara15@users.noreply.github.com>
2023-10-27 16:06:45 -0300

    jjvbfdb

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function TocaSomPom(){
+function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_som').onclick =tocaSomPom;
-    
+document.querySelector('.tecla_som').onclick = TocaSomPom;
+
 

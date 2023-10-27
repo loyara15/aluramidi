f634d020e3485104f099a9e087ffb0bc551761e9
loyara15 <104777662+loyara15@users.noreply.github.com>
2023-10-27 16:13:02 -0300

    vnbn

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-
-document.querySelector('.tecla_som').onclick = tocaSomPom;
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_som').onclick = tocaSomClap;
-
+document.querySelector('.tecla_pom').onclik = tocaSomPom;
